b3141e0183658b25bd5e7c8bb26a161e3c747b58
Vera Plus1 <vps.source1@outlook.com>
2020-12-24 15:49:14 +0700

    [ADD.nes] All Contract

---
+++ CALLSCREEN_UI/Components/AllContract/Models/Contract.cs
@@ -0,0 +1,18 @@
+﻿namespace CALLSCREEN_UI.Components.AllContract.Models
+{
+    public class Contract
+    {
+        public string biz { get; set; }
+        public string due { get; set; }
+        public string contractNo { get; set; }
+        public string callCenterID { get; set; }
+        public string contractDate { get; set; }
+        public string principal { get; set; }
+        public string osBal { get; set; }
+        public string closingAmt { get; set; }
+        public string conStatus { get; set; }
+        public string odStatus { get; set; }
+        public string excessAmt { get; set; }
+        public string productName { get; set; }
+    }
+}
