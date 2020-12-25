e2d10b3ee74ae6596862583d2556c724a3d72e29
SpeeDHacKe <SpeeDHacKe@hotmail.com>
2020-12-25 19:00:49 +0700

    [ADD] : CustomerInformation

---
+++ CALLSCREEN_UI/Components/CustomerInformation/Models/ReferencesModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CALLSCREEN_UI.Components.CustomerInformation.Models
+{
+    public class ReferencesModel
+    {
+        public string title { get; set; }
+        public string name { get; set; }
+        public string relationship { get; set; }
+    }
+}
---
+++ CALLSCREEN_UI/Components/CustomerInquiry/Models/CustomerModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CALLSCREEN_UI.Components.CustomerInquiry.Models
+{
+    public class CustomerModel
+    {
+        public int? id { get; set; }
+        public string idNo { get; set; }
+        public string title { get; set; }
+        public string thaiName { get; set; }
+        public string thaiSurname { get; set; }
+    }
+}
